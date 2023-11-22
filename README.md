d818cdfb1e64be7a46190aec8d12e60b4a0626fd
abhinandpn <abhinandpn4067@gmail.com>
2023-11-23 02:30:42 +0530

    viper with json read data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Viper-Golang
+
+```
+Golang viper is a package that helps to provide full configuration to an application in Golang with 12-factor apps. Handling and updating configuration for a large and complex program, such as creating a server application or any other application that relies heavily on user configuration manipulation, is a difficult undertaking. Furthermore, current programs are designed to run in a variety of settings, such as Docker, cloud infrastructures, and so forth.
+```
--- main.go
+++ main.go
@@ -7,9 +7,26 @@ import (
 )
 
 func main() {
+
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 
-	fmt.Println("port is : ", viper.Get("PORT"))
-	fmt.Println("Username is : ", viper.Get("USER_NAME"))
+	viper.AddConfigPath(".")
+	viper.SetConfigName("sample") // Register config file name (no extension) only name for e.g sample
+	viper.SetConfigType("json")   // Look for specific type
+	viper.ReadInConfig()
+
+	var config ConfigExample
+
+	viper.Unmarshal(&config)
+
+	fmt.Println(config.CompilerOptions.Target)
+}
+
+type ConfigExample struct {
+	CompilerOptions struct {
+		Module string `json:"module"`
+		Target string `json:"target"`
+	} `json:"compilerOptions"`
+	Exclude []string `json:"exclude"`
 }
